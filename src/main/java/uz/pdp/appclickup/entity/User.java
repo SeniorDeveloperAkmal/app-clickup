ac2ca86ff1f3536041d4a86077f46252f1faa89e
AkmalMaxmudov <mahmudovakmaljon70@gmail.com>
2022-02-03 19:03:27 +0500

    All bug fixed

--- src/main/java/uz/pdp/appclickup/entity/User.java
+++ src/main/java/uz/pdp/appclickup/entity/User.java
@@ -2,23 +2,84 @@ package uz.pdp.appclickup.entity;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.GenericGenerator;
-import org.hibernate.annotations.Type;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import uz.pdp.appclickup.entity.enums.SystemRoleName;
+import uz.pdp.appclickup.entity.template.AbsUUIDEntity;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import java.util.UUID;
+import javax.persistence.*;
+import java.util.Collection;
+import java.util.Collections;
 
+@EqualsAndHashCode(callSuper = true)
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Entity
-public class User {
-    @Id
-    @GeneratedValue(generator = "uuid2")
-    @Type(type = "org.hibernate.PostgresUUIDType")
-    @GenericGenerator(name = "uuid2", strategy = "org.hibernate.id.UUIDGenerator")
-    private UUID id;
+@Entity(name = "users")
+public class User extends AbsUUIDEntity implements UserDetails {
+
+    private String fullName;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    private String password;
+
+    private String color;
+
+    private String initialLetter;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    private Attachment avatar;
+
+    private boolean enabled;
+    private boolean accountNonExpired = true;
+    private boolean accountNonLocked = true;
+    private boolean credentialsNonExpired = true;
+
+    @Enumerated(EnumType.STRING)
+    private SystemRoleName systemRoleName;
+
+    private String emailCode;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        SimpleGrantedAuthority simpleGrantedAuthority = new SimpleGrantedAuthority(this.systemRoleName.name());
+        return Collections.singletonList(simpleGrantedAuthority);
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return this.accountNonExpired;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return this.accountNonLocked;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return this.credentialsNonExpired;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return this.enabled;
+    }
+
+    public User(String fullName, String email, String password, SystemRoleName systemRoleName) {
+        this.fullName = fullName;
+        this.email = email;
+        this.password = password;
+        this.systemRoleName = systemRoleName;
+    }
 }
